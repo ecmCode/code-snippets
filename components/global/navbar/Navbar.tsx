5c4bdaa5bc89fbc5b073fdb3eb1743a756b1b6bb
ecmcode <ecmcode.dev@gmail.com>
2023-08-04 21:38:06 -0500

    chore(components ): Navbar routing

--- components/global/navbar/Navbar.tsx
+++ components/global/navbar/Navbar.tsx
@@ -1,12 +1,25 @@
+"use client"
+
 import Link from "next/link";
+import style from "./Navbar.module.css"
+import { usePathname } from "next/navigation";
+
+const routes = [
+  { name: "Home", path: "/" },
+  { name: "Snippets", path: "/snippets" },
+  { name: "About", path: "/about" },
+]
 
 const Navbar = () => {
+  const pathname = usePathname()
   return (
     <nav>
-      <div className="flex gap-4 my-4">
-        <Link href="/">Home</Link>
-        <Link href="/snippets">Snippets</Link>
-        <Link href="/about">About</Link>
+      <div className={style.navbar}>
+        {routes.map(route => (
+          <Link key={route.name} href={route.path} className={route.path === pathname ? "text-cyan-400" : "hover:text-cyan-400"}>
+            {route.name}
+          </Link>
+        ))}
       </div>
     </nav>
   );
