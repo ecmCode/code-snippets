489c569cf439b222ec6340c3ab25d73db44999db
ecmcode <ecmcode.dev@gmail.com>
2023-08-06 19:57:24 -0500

    feat(navbar): respnisive navbar

--- components/global/navbar/Navbar.tsx
+++ components/global/navbar/Navbar.tsx
@@ -1,8 +1,11 @@
 "use client"
 
+import {GiHamburgerMenu} from "react-icons/gi"
+
 import Link from "next/link";
 import style from "./Navbar.module.css"
 import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const routes = [
   { name: "Home", path: "/" },
@@ -12,15 +15,33 @@ const routes = [
 
 const Navbar = () => {
   const pathname = usePathname()
+  const [menuOpen,setMenuOpen] = useState(false)
+  
+  useEffect(() => {
+    setMenuOpen(false)
+  }, [pathname])
+  
   return (
-    <nav>
-      <div className={style.navbar}>
-        {routes.map(route => (
-          <Link key={route.name} href={route.path} className={route.path === pathname ? "text-cyan-400" : "hover:text-cyan-400"}>
-            {route.name}
-          </Link>
-        ))}
-      </div>
+    <nav className="my-4 flex-none w-full">
+        <div className="relative visible md:hidden mx-10 text-2xl float-right">
+          <button onClick={() => setMenuOpen(prev => !prev)} className={menuOpen ? "text-cyan-400" : "hover:text-cyan-400"}>
+            <GiHamburgerMenu />
+          </button>
+          <div className={`${menuOpen ? "visible": "hidden"} absolute top-4 right-0 flex flex-col gap-2 p-2 text-right`}>
+            {routes.map(route => (
+              <Link key={route.name} href={route.path} className={route.path === pathname ? "text-cyan-400" : "hover:text-cyan-400"}>
+                {route.name}
+              </Link>
+            ))}
+          </div>
+        </div>
+        <div className={style.navbar}>
+            {routes.map(route => (
+              <Link key={route.name} href={route.path} className={route.path === pathname ? "text-cyan-400" : "hover:text-cyan-400"}>
+                {route.name}
+              </Link>
+            ))}
+        </div>
     </nav>
   );
 };
